819e860c1a6545ee7bb19cbbcb03bc3c8f4bb236
MonkiDrufi1010 <monkidrufi6497@gmail.com>
2023-04-13 12:57:10 +0800

    Initial Commit

---
+++ MyFirstApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by 謝榮駿 on 2023/4/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
