02020792b4d790e94273050935071f65ebf41be0
MonkiDrufi1010 <monkidrufi6497@gmail.com>
2023-04-14 15:03:00 +0800

    0416 finish App

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    
+    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var myLabel: UILabel!
+    
+    
+    let message = ["You Are Awesome",
+                   "You Are Great",
+                   "You Are Fantastic",
+                   "Fabulous? That's You!",
+                   "How Are You Today",
+                   "How mach" ]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func showBtn(_ sender: UIButton) {
+        
+        myLabel.text = message[Int.random(in: 0...message.count - 1)]
+        
+        myImageView.image = UIImage(named: "image\(Int.random(in: 0...9))")
+        
+    }
+    
 }
 
