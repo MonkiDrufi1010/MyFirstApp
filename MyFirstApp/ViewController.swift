166922e6673987294a3b8d3e04534f561baf6f78
MonkiDrufi1010 <monkidrufi6497@gmail.com>
2023-04-17 15:55:13 +0800

    加上聲音 fun soundPlay()

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -20,40 +21,72 @@ class ViewController: UIViewController {
                    "How Are You Today",
                    "How mach" ]
     
-    var imageNumber = 0
-    var messageNumber = 0
+    var imageNumber = -1
+    var messageNumber = -1
+    var soundNumber = -1
+    var totalNumberSound = 6
     var totalNumberOfImage = 9
+    var audioPlayer: AVAudioPlayer!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    func playSound(num: Int) {
+        if let sound = NSDataAsset(name: "sound\(num)") {
+            do {
+                try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
+            } catch {
+                print("Error: \(error.localizedDescription)")
+            }
+        }else {
+            print("error haha")
+        }
+        
+    }
+    
+    
     @IBAction func showBtn(_ sender: UIButton) {
         
-        var newMessage = Int.random(in: 0...message.count-1)
-        while messageNumber == newMessage {
+        var newMessage: Int
+       
+        repeat {
             newMessage = Int.random(in: 0...message.count-1)
             print("messageNumber: \(messageNumber)")
-//            myLabel.text = message[newMessage]
-        
-        
-        
-        
-        
-        
+            print("這裡當嗎")
+           
+        } while messageNumber == newMessage
         messageNumber = newMessage
-     
-        print("這裡當嗎")
         myLabel.text = message[messageNumber]
-        var newImageView = Int.random(in: 0...totalNumberOfImage)
-        while imageNumber == newImageView {
+      
+       
+        var newImageView: Int
+        repeat {
             newImageView = Int.random(in: 0...totalNumberOfImage)
-        }
+            
+        } while imageNumber == newImageView
         imageNumber = newImageView
         myImageView.image = UIImage(named: "image\(newImageView)")
         
+        var newSoundPlayer: Int
+        repeat {
+            newSoundPlayer = Int.random(in: 0...totalNumberSound)
+            
+        } while soundNumber == newSoundPlayer
+        
+        soundNumber = newSoundPlayer
+        playSound(num: soundNumber)
+        
+        
+        
+       
     }
+    
+
+    
     
 }
 
